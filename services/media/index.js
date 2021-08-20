fef0c046ef9ca38f26842c90c4e50a6e3d28f1e8
Koulinn <drdverzola@gmail.com>
2021-08-20 20:11:25 +0100

    upload img to cloudinary working

--- services/media/index.js
+++ services/media/index.js
@@ -1,6 +1,8 @@
 import  express  from "express";
-import {getOMDBData, checkMovieOnJSON, editMovieText, addMovieJSON, deleteMovie, addReview} from '../../lib/service-utils.js'
+import {getOMDBData, checkMovieOnJSON, editMovieText, addMovieJSON, deleteMovie, addReview, addPosterToJSON} from '../../lib/service-utils.js'
 import {movieFieldsValidation} from '../../lib/validations.js'
+import multer from "multer";
+import { cloudinaryStorage } from "../../lib/export-utils.js";
 
 
 
@@ -10,6 +12,8 @@ mediaRouter.get("/", checkMovieOnJSON, getOMDBData)
 mediaRouter.put('/:id', editMovieText)
 mediaRouter.post('/', addMovieJSON)
 mediaRouter.delete('/:id', deleteMovie)
+// POSTER
+mediaRouter.post('/:id/poster', multer({storage:cloudinaryStorage}).single('Poster'), addPosterToJSON)
 
 
 
