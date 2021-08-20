1a099021223bfe83eb9f36d8775917aebaadf802
Koulinn <drdverzola@gmail.com>
2021-08-20 12:15:24 +0100

    fixed search and created put

--- services/media/index.js
+++ services/media/index.js
@@ -6,5 +6,6 @@ import {getOMDBData, checkMovieOnJSON} from '../../lib/service-utils.js'
 const mediaRouter = express.Router()
 
 mediaRouter.get("/", checkMovieOnJSON, getOMDBData)
+mediaRouter.put('/:id')
 
 export default mediaRouter
