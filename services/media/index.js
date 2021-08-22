69fdcd3a228de8d48f9f631eb06a25bb161d93cb
Koulinn <drdverzola@gmail.com>
2021-08-22 15:36:11 +0100

    Added get Single movie route

--- services/media/index.js
+++ services/media/index.js
@@ -1,6 +1,10 @@
-import  express  from "express";
-import {getOMDBData, checkMovieOnJSON, editMovieText, addMovieJSON, deleteMovie, addReview, addPosterToJSON, deleteReview } from '../../lib/service-utils.js'
-import {movieFieldsValidation, commentValidation} from '../../lib/validations.js'
+import express from "express";
+import {
+    getOMDBData, checkMovieOnJSON, editMovieText,
+    addMovieJSON, deleteMovie, addReview, addPosterToJSON,
+    deleteReview, getSingleMovie
+    } from '../../lib/service-utils.js'
+import { movieFieldsValidation, commentValidation } from '../../lib/validations.js'
 import multer from "multer";
 import { cloudinaryStorage, createPDFPipeline } from "../../lib/export-utils.js";
 // import {requestSpeedLimiter} from '../../lib/server-config.js'
@@ -17,7 +21,8 @@ mediaRouter.route('/')
 mediaRouter.route('/:id')
     .put(editMovieText)
     .delete(deleteMovie)
-    // .post('/:id/poster', multer({storage:cloudinaryStorage}).single('Poster'), addPosterToJSON)
+    .get(getSingleMovie)
+// .post('/:id/poster', multer({storage:cloudinaryStorage}).single('Poster'), addPosterToJSON)
 
 
 // mediaRouter.get("/", checkMovieOnJSON, getOMDBData)
@@ -25,7 +30,7 @@ mediaRouter.route('/:id')
 // mediaRouter.post('/', addMovieJSON)
 // mediaRouter.delete('/:id', deleteMovie)
 // POSTER
-mediaRouter.post('/:id/poster', multer({storage:cloudinaryStorage}).single('Poster'), addPosterToJSON)
+mediaRouter.post('/:id/poster', multer({ storage: cloudinaryStorage }).single('Poster'), addPosterToJSON)
 
 //Reviews
 mediaRouter.post('/:id/review', commentValidation, addReview)
