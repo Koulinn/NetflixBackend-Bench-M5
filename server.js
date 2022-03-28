66803f7e091702027888a16eaeb914d183068d7f
Rafael Lima <drdverzola@gmail.com>
2022-03-28 19:56:14 +0100

    Fix minor issues

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
-import express from 'express'
-import cors from 'cors'
-import {corsConfig, requestSpeedLimiter} from './lib/server-config.js'
-import mediaRouter from './services/media/index.js'
-import { notFoundErrorHandler, badRequestErrorHandler, serverErrorHandler, forbiddenRequest } from './lib/error-Handlers.js'
+import express from "express";
+import cors from "cors";
+import { corsConfig, requestSpeedLimiter } from "./lib/server-config.js";
+import mediaRouter from "./services/media/index.js";
+import {
+  notFoundErrorHandler,
+  badRequestErrorHandler,
+  serverErrorHandler,
+  forbiddenRequest,
+} from "./lib/error-Handlers.js";
 
+const server = express();
 
-const server = express()
-
-
-server.use(requestSpeedLimiter)
-server.use(express.json())
-server.use(cors(corsConfig))
-server.use('/media', mediaRouter)
-
+server.use(requestSpeedLimiter);
+server.use(express.json());
+server.use(cors(corsConfig));
+server.use("/media", mediaRouter);
 
 // Errors middlewares
-server.use(notFoundErrorHandler)
-server.use(forbiddenRequest)
-server.use(badRequestErrorHandler)
-server.use(serverErrorHandler)
+server.use(notFoundErrorHandler);
+server.use(forbiddenRequest);
+server.use(badRequestErrorHandler);
+server.use(serverErrorHandler);
 
+const port = process.env.PORT;
+server.listen(port || 3003, () => {
+  console.log("Server running port = " + port);
+});
 
-
-
-const port = process.env.PORT
-server.listen(port, ()=>{
-    console.log('Server running port = ' + port)
-})
-
-server.on('error', (err)=> console.log(err))
+server.on("error", (err) => console.log(err));
--- services/media/index.js
+++ services/media/index.js
@@ -1,44 +1,57 @@
 import express from "express";
 import {
-    getOMDBData, checkMovieOnJSON, editMovieText,
-    addMovieJSON, deleteMovie, addReview, addPosterToJSON,
-    deleteReview, getSingleMovie
-    } from '../../lib/service-utils.js'
-import { movieFieldsValidation, commentValidation } from '../../lib/validations.js'
+  getOMDBData,
+  checkMovieOnJSON,
+  editMovieText,
+  addMovieJSON,
+  deleteMovie,
+  addReview,
+  addPosterToJSON,
+  deleteReview,
+  getSingleMovie,
+} from "../../lib/service-utils.js";
+import {
+  movieFieldsValidation,
+  commentValidation,
+} from "../../lib/validations.js";
 import multer from "multer";
-import { cloudinaryStorage, createPDFPipeline } from "../../lib/export-utils.js";
+import {
+  cloudinaryStorage,
+  createPDFPipeline,
+} from "../../lib/export-utils.js";
 // import {requestSpeedLimiter} from '../../lib/server-config.js'
 
+const mediaRouter = express.Router();
 
+mediaRouter
+  .route("/")
+  // .get(requestSpeedLimiter, checkMovieOnJSON, getOMDBData)
+  .get(checkMovieOnJSON, getOMDBData)
+  .post(movieFieldsValidation, addMovieJSON);
 
-const mediaRouter = express.Router()
-
-mediaRouter.route('/')
-    // .get(requestSpeedLimiter, checkMovieOnJSON, getOMDBData)
-    .get(checkMovieOnJSON, getOMDBData)
-    .post(movieFieldsValidation, addMovieJSON)
-
-mediaRouter.route('/:id')
-    .put(editMovieText)
-    .delete(deleteMovie)
-    .get(getSingleMovie)
+mediaRouter
+  .route("/:id")
+  .put(editMovieText)
+  .delete(deleteMovie)
+  .get(getSingleMovie);
 // .post('/:id/poster', multer({storage:cloudinaryStorage}).single('Poster'), addPosterToJSON)
-
 
 // mediaRouter.get("/", checkMovieOnJSON, getOMDBData)
 // mediaRouter.put('/:id', editMovieText)
 // mediaRouter.post('/', addMovieJSON)
 // mediaRouter.delete('/:id', deleteMovie)
 // POSTER
-mediaRouter.post('/:id/poster', multer({ storage: cloudinaryStorage }).single('Poster'), addPosterToJSON)
+mediaRouter.post(
+  "/:id/poster",
+  multer({ storage: cloudinaryStorage }).single("Poster"),
+  addPosterToJSON
+);
 
 //Reviews
-mediaRouter.post('/:id/review', commentValidation, addReview)
-mediaRouter.delete('/:id/review/:reviewID', deleteReview)
+mediaRouter.post("/:id/review", commentValidation, addReview);
+mediaRouter.delete("/:id/review/:reviewID", deleteReview);
 
 // DownloadPDF
-mediaRouter.get('/:id/pdf', createPDFPipeline)
+mediaRouter.get("/:id/pdf", createPDFPipeline);
 
-
-
-export default mediaRouter
+export default mediaRouter;
